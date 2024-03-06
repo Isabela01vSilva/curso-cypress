42c2b3e65e3632395bd469644bd22c62fae6ead5
isabela.vsilva@bandtec.com.br <isabela.vsilva@bandtec.com.br>
2024-03-06 12:51:22 -0300

    testando registro

--- curso-cypress/cypress/e2e/testando.cy.js
+++ curso-cypress/cypress/e2e/testando.cy.js
@@ -1,5 +1,11 @@
 describe('template spec', () => {
   it('passes', () => {
     cy.visit('http://localhost:4200/#/home')
+    cy.contains('a', 'Register now').click();
+    cy.get('[formcontrolname="email"]').type('isabela@email.com.br')
+    cy.get('[formcontrolname="fullName"]').type('Isabela Pessoa')
+    cy.get('[formcontrolname="userName"]').type('isabelanap')
+    cy.get('[formcontrolname="password"]').type('isabela123')
+    cy.contains('button', 'Register').click();
   })
 })
