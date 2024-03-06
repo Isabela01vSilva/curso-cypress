2c24fd1cc4d04f29521304c23e467f74eaed37d9
isabela.vsilva@bandtec.com.br <isabela.vsilva@bandtec.com.br>
2024-03-06 14:32:37 -0300

    melhorias no teste da tela register (caminho feliz)

--- curso-cypress/cypress/e2e/testando.cy.js
+++ curso-cypress/cypress/e2e/testando.cy.js
@@ -1,11 +1,11 @@
-describe('template spec', () => {
-  it('passes', () => {
+describe('Pagina de cadastro', () => {
+  it('Preencher os campos do formulario corretamente para cadastrar um novo usuario', () => {
     cy.visit('http://localhost:4200/#/home')
     cy.contains('a', 'Register now').click();
-    cy.get('[formcontrolname="email"]').type('isabela@email.com.br')
-    cy.get('[formcontrolname="fullName"]').type('Isabela Pessoa')
-    cy.get('[formcontrolname="userName"]').type('isabelanap')
-    cy.get('[formcontrolname="password"]').type('isabela123')
+    cy.get('[data-test="email"]').type('isabela@email.com.br')
+    cy.get('[data-test="fullName"]').type('Isabela Pessoa')
+    cy.get('[data-test="registerUserName"]').type('isabelanap')
+    cy.get('[data-test="registerPassword"]').type('isabela123')
     cy.contains('button', 'Register').click();
   })
 })
