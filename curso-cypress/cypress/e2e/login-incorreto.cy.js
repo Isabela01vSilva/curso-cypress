ff43276851cae8c27a514e6a287f2e3aa311adf5
isabela.vsilva@bandtec.com.br <isabela.vsilva@bandtec.com.br>
2024-03-07 16:30:56 -0300

    criando um duble na tela login com informacoes incorretas

--- curso-cypress/cypress/e2e/login-incorreto.cy.js
+++ curso-cypress/cypress/e2e/login-incorreto.cy.js
@@ -1,7 +1,19 @@
 describe('Pagina de login', () => {
-    it('Verificar mensagem de campos obrigatorios', () => {
-      cy.visit('http://localhost:4200/#/home')
-      cy.contains('User name is required!').should('be.visible');
-      cy.contains('Password is required!').should('be.visible');
-    })
+
+  beforeEach(() => {
+    cy.visit('http://localhost:4200/#/home')
+    cy.intercept('POST', 'http://localhost:3000/user/login', {
+      statusCode: 400
+    }).as('stubPost')
   })
+
+  it('Verificar mensagem de campos obrigatorios', () => {
+    cy.contains('User name is required!').should('be.visible');
+    cy.contains('Password is required!').should('be.visible');
+  })
+
+  it('Deve falhar mesmo que os campos sejam preenchidos corretamente', () => {
+    cy.login('isabelanap', 'isabela123')
+    cy.wait('@stubPost')
+  })
+})
