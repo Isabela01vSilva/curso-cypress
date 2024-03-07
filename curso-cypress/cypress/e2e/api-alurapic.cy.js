d3ce41c23ef8a985a9f0b2d10d318aa0f6cffb58
isabela.vsilva@bandtec.com.br <isabela.vsilva@bandtec.com.br>
2024-03-07 17:20:38 -0300

    requisicoes em api

--- curso-cypress/cypress/e2e/api-alurapic.cy.js
+++ curso-cypress/cypress/e2e/api-alurapic.cy.js
@@ -1,4 +1,4 @@
-describe('Buscando dados ', () => {
+ describe('Buscando dados ', () => {
     it('Dados do usuario', () => {
         cy.request({
             method: 'POST',
@@ -9,6 +9,20 @@ describe('Buscando dados ', () => {
             expect(res.body).is.not.empty
             expect(res.body).to.have.property('id')
             expect(res.body.id).to.be.equal(3)
+        })
+    })
+}) 
+
+describe('Fotos do usuario', () => {
+    it('Dados do usuario', () => {
+        cy.request({
+            method: 'GET',
+            url: 'http://localhost:3000/isabelanap/photos',
+        }).then((res) => {
+            expect(res.status).to.be.equal(200)
+            expect(res.body).is.not.empty
+            expect(res.body[0]).to.have.property('description')
+            expect(res.body[0].description).to.be.equal('porquinho')
         })
     })
 })
